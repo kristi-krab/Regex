5b2ea0990467034bdb5afd4137b8fe5a6420dcfb
kristi-krab <124806853+kristi-krab@users.noreply.github.com>
2023-03-02 22:43:50 +0300

    Add files via upload

---
+++ Time/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class Main {
+    public static void main(String[] args) {
+        String text = "Завтрак в 09:00";
+        Pattern pattern = Pattern.compile("([01]?[0-9]|2[0-3]):[0-5][0-9]");
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find())
+        {
+            System.out.println(text.substring(matcher.start(),matcher.end()));
+        }
+    }
+}
